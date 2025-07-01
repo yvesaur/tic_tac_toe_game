a23fbfcccfbb3aa75a14c85a54b9ecac411bbf06
Yves Casio <yveschristianpcasio@gmail.com>
2025-07-01 20:01:19 +0800

    Base objects for Player, game board, and game controller

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,22 @@
 console.log("Test JS script... working");
+
+function createPlayer(name, selectedMarker) {
+	function makeMove(movePosition) {
+		gameBoard.board.push(`${selectedMarker}-${movePosition}`);
+	}
+
+	return { name, selectedMarker, makeMove };
+}
+
+const gameBoard = (function () {
+	const boardColumnAndRow = 3;
+	const board = [];
+
+	return { board };
+})();
+
+const gameController = (function () {
+	const player1 = createPlayer("Tom", "O");
+	const player2 = createPlayer("Jerry", "X");
+	return { player1, player2 };
+})();
